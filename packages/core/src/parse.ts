5759c9c738c08b856c21094039215167d3c4491c
vanyauhalin <vanyauhalin@gmail.com>
2022-02-19 20:10:34 +0300

    feat: match elements in parse fn

--- packages/core/src/parse.ts
+++ packages/core/src/parse.ts
@@ -1,7 +1,36 @@
+export class Element {
+  tag: string;
+
+  constructor({ tag }: { tag: string }) {
+    this.tag = tag;
+  }
+}
+
 export function parse(content?: string) {
   if (!content) {
     throw new Error('Content must be a string');
   }
 
-  return content;
+  const rawElements = content.matchAll(/<(?<open>\w+)>|<\/(?<close>\w+)>/g);
+
+  if (rawElements) {
+    const elements = [] as Element[];
+
+    [...rawElements].forEach((rawElement) => {
+      if (rawElement.groups) {
+        const { open, close } = rawElement.groups;
+        const tag = open || close;
+
+        if (tag) {
+          elements.push(new Element({
+            tag,
+          }));
+        }
+      }
+    });
+
+    return elements;
+  }
+
+  throw new Error();
 }

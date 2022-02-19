27c8fd7a0a1b41a20c9660e97fd92d9494d09295
vanyauhalin <vanyauhalin@gmail.com>
2022-02-19 20:09:13 +0300

    test: parse simple html element

--- test/core/parse.ts
+++ test/core/parse.ts
@@ -1,9 +1,20 @@
-import { parse } from '@mijelangelo/core/src/parse';
+import { Element, parse } from '@mijelangelo/core/src/parse';
 import { test } from 'uvu';
-import { throws } from 'uvu/assert';
+import { equal, throws } from 'uvu/assert';
 
 test('if content is empty', () => {
   throws(parse, Error);
+});
+
+test('parse simple html element', () => {
+  equal(parse('<div></div>'), [
+    new Element({
+      tag: 'div',
+    }),
+    new Element({
+      tag: 'div',
+    }),
+  ]);
 });
 
 test.run();
